a93f4bcdfec834106a002f92fc9135648dc0b409
Jungnahyeon <nahyun4669@gmail.com>
2024-12-02 00:28:46 +0900

    Update authentication.js

--- assets/js/authentication.js
+++ assets/js/authentication.js
@@ -1,15 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     // Firebase 구성 정보 및 초기화
     const firebaseConfig = {
-    apiKey: "AIzaSyAHSA0O4V9vnc5E1YvQNpQTZT5z4QOWE1k",
-    authDomain: "software-89c07.firebaseapp.com",
-    databaseURL: "https://software-89c07-default-rtdb.firebaseio.com",
-    projectId: "software-89c07",
-    storageBucket: "software-89c07.appspot.com",
-    messagingSenderId: "567506244346",
-    appId: "1:567506244346:web:a22bba69dd21ae59146f43",
-    measurementId: "G-KQT14W936Y"
-};
+        apiKey: "AIzaSyAHSA0O4V9vnc5E1YvQNpQTZT5z4QOWE1k",
+        authDomain: "software-89c07.firebaseapp.com",
+        databaseURL: "https://software-89c07-default-rtdb.firebaseio.com",
+        projectId: "software-89c07",
+        storageBucket: "software-89c07.appspot.com",
+        messagingSenderId: "567506244346",
+        appId: "1:567506244346:web:a22bba69dd21ae59146f43",
+        measurementId: "G-KQT14W936Y"
+    };
 
     // Firebase 초기화
     if (!firebase.apps.length) {
