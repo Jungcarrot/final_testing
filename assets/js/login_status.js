a4e488f25d435c92b7aa9779486f076c50bae7f8
Jungnahyeon <nahyun4669@gmail.com>
2024-12-02 03:43:39 +0900

    Update login_status.js

--- assets/js/login_status.js
+++ assets/js/login_status.js
@@ -1,3 +1,6 @@
+import { database } from "./DB.js"; // 필요시 사용하기 위해 import
+// 이미 로그인한 상태인지 확인하는 로직 추가
+
 document.addEventListener("DOMContentLoaded", () => {
     const loggedInUI = document.getElementById("logged-in-ui");
     const loggedOutUI = document.getElementById("logged-out-ui");
@@ -14,6 +17,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+// 로그아웃 이벤트 추가
 document.getElementById("logout")?.addEventListener("click", () => {
     localStorage.removeItem("loggedIn"); // 로그인 상태 제거
     window.location.href = "index.html"; // index.html로 리다이렉트
