a4ee9ac04f55049f710a0eac0396cbfb5975f952
Jungnahyeon <nahyun4669@gmail.com>
2024-12-02 00:50:34 +0900

    Update DB.js

--- assets/js/DB.js
+++ assets/js/DB.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/11.0.2/firebase-database.js";
+
 // Firebase 구성 정보
 const firebaseConfig = {
     apiKey: "AIzaSyAHSA0O4V9vnc5E1YvQNpQTZT5z4QOWE1k",
@@ -11,15 +14,16 @@ const firebaseConfig = {
 };
 
 // Firebase 초기화
-firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
 // Firebase 연결 확인
-const connectedRef = database.ref('.info/connected');
-connectedRef.on('value', (snapshot) => {
+const connectedRef = ref(database, '.info/connected');
+onValue(connectedRef, (snapshot) => {
     if (snapshot.val() === true) {
         console.log('Firebase에 연결되었습니다.');
     } else {
         console.log('Firebase 연결이 끊어졌습니다.');
     }
 });
+
