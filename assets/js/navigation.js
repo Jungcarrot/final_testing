1e58fd17b18bc786a9f7c9056525eae2cbe9fff7
LeeMyeongIn <dulsfnj413@gmail.com>
2024-12-02 01:41:47 +0900

    Update navigation.js

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,8 +1,8 @@
 function navigateToPage(page) {
-    console.log(`페이지로 이동 중: ${page}`);
+    console.log(Navigating to: ${page});
     window.location.href = page; // 지정된 페이지로 이동
 }
 
 function navigateToHome() {
-    window.location.href = "home.html"; // 홈 페이지로 이동
+    window.location.href = "index.html"; // 홈으로 이동
 }
