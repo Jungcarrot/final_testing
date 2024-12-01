3aa66baf2686b78ba6a42a46a70f94ecfdd2e9a6
LeeMyeongIn <dulsfnj413@gmail.com>
2024-12-02 01:51:11 +0900

    Update logout.js

--- assets/js/logout.js
+++ assets/js/logout.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", () => {
         fetch("/api/logout", { method: "POST" });
 
         // 로컬 로그인 상태 제거
-        localStorage.removeItem("userLoggedIn");
-        localStorage.removeItem("username");
+        localStorage.removeItem("loggedIn"); // 'userLoggedIn'을 'loggedIn'으로 변경하여 근본과 맞춤
+        localStorage.removeItem("nickName"); // 'username'을 'nickName'으로 변경하여 근본과 맞춤
         alert("로그아웃되었습니다.");
         window.location.href = "index.html";
     });
