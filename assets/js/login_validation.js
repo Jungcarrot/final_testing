5a2b4e748b3b19579a20af0f08ce8e79481de25d
LeeMyeongIn <dulsfnj413@gmail.com>
2024-12-02 01:52:08 +0900

    Update login_validation.js

--- assets/js/login_validation.js
+++ assets/js/login_validation.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const loginForm = document.querySelector("form");
 
     loginForm.addEventListener("submit", (event) => {
-        const username = document.getElementById("login-username").value.trim();
+        const loginID = document.getElementById("login-username").value.trim(); // 'username'을 'loginID'로 변경하여 근본과 맞춤
         const password = document.getElementById("login-password").value.trim();
 
-        if (!username || !password) {
+        if (!loginID || !password) {
             event.preventDefault();
             alert("아이디와 비밀번호를 모두 입력해주세요.");
         }
