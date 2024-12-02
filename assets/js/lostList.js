decb5bced125200e6fdf0d4173b1b3d205661aa2
LeeMyeongIn <dulsfnj413@gmail.com>
2024-12-03 04:35:27 +0900

    Update lostList.js

--- assets/js/lostList.js
+++ assets/js/lostList.js
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', () => {
                     const row = `
                         <tr>
                             <td>${index + 1}</td> <!-- 번호 -->
-                            <td><a href="findPost.html?pid=${post.id}">${post.title}</a></td> <!-- 제목 수정됨 -->
+                            <td><a href="findPost.html?pid=${post.id}">${post.title}</a></td> <!-- 제목 -->
                             <td>${post.authorNickname}</td> <!-- 작성자 닉네임 -->
                             <td>${post.date}</td> <!-- 작성일 -->
                         </tr>
